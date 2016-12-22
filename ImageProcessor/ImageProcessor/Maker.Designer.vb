440da6b9c8e1a468336760f45f3b71dc5486adf7
Hero Lee <Hero Lee>
2016-12-21 21:27:48 -0500

    Initial commit

---
+++ ImageProcessor/ImageProcessor/Maker.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Maker
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFileDirectory = New System.Windows.Forms.TextBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtFileDirectory
+        '
+        Me.txtFileDirectory.Location = New System.Drawing.Point(111, 145)
+        Me.txtFileDirectory.Name = "txtFileDirectory"
+        Me.txtFileDirectory.Size = New System.Drawing.Size(302, 29)
+        Me.txtFileDirectory.TabIndex = 0
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(199, 192)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(126, 63)
+        Me.btnBrowse.TabIndex = 1
+        Me.btnBrowse.Text = "Browse"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'Maker
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(511, 440)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.txtFileDirectory)
+        Me.Name = "Maker"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtFileDirectory As TextBox
+    Friend WithEvents btnBrowse As Button
+End Class
---
+++ ImageProcessor/ImageProcessor/Maker.vb
@@ -0,0 +1,35 @@
+﻿Public Class Maker
+    Private Sub btnBrowse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBrowse.Click
+        Dim myFileDlog As New OpenFileDialog()
+        'look for files in the C drive
+        myFileDlog.InitialDirectory = "c:\"
+        'specifies what type of data files to look for
+        myFileDlog.Filter = ".png Files (*.png)|*.png" & "|.jpg Files (*.jpg)|*.jpg"
+        'myFileDlog.Filter = "All Files (*.*)|*.*" &
+        '    "|Data Files (*.dat)|*.dat"
+        'specifies which data type is focused on start up
+        myFileDlog.FilterIndex = 1
+        'Gets or sets a value indicating whether the dialog box restores the current directory before closing.
+        myFileDlog.RestoreDirectory = True
+
+        'seperates message outputs for files found or not found
+        If myFileDlog.ShowDialog() =
+            DialogResult.OK Then
+            If Dir(myFileDlog.FileName) <> "" Then
+                'MsgBox("File Exists: " &
+                'myFileDlog.FileName,
+                'MsgBoxStyle.Information)
+            Else
+                MsgBox("File Not Found",
+                       MsgBoxStyle.Critical)
+            End If
+        End If
+
+        'Adds the file directory to the text box
+        txtFileDirectory.Text = myFileDlog.FileName
+    End Sub
+
+    Private Sub Maker_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
